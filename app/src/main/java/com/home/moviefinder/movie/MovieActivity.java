1b0ffc6b8037cbd5b1356c6d3cb561d191834e8f
teixeira007 <viniciusteixeiravini16@gmail.com>
2023-05-10 22:55:13 -0300

    ToolBar

--- app/src/main/java/com/home/moviefinder/movie/MovieActivity.java
+++ app/src/main/java/com/home/moviefinder/movie/MovieActivity.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.home.moviefinder.R;
 
-public class MovieActivity extends Activity {
+public class MovieActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+
     }
 }
